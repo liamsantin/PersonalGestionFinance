03b1f8844c033767e24dc7f1c5085cdaf9ce1fe1
Liam Santin <lsantin312005@gmail.com>
2025-09-11 09:28:26 +0200

    add data of user entities

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,6 +1,52 @@
-﻿namespace ApiPersonalGestionFinance.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace ApiPersonalGestionFinance.Entities;
+
+[Table("TA_USER")]
 public class User
 {
+    [Key]
+    [Column("user_id")]
+    public int UserId { get; set; }
+
+    [Required]
+    [Column("user_nom")]
+    public string Nom { get; set; }
+
+    [Required]
+    [Column("user_prenom")]
+    public string Prenom { get; set; }
+
+    [Required]
+    [Column("user_email")]
+    public string Email { get; set; }
+
+    [Required]
+    [Column("user_password")]
+    public string Password { get; set; }
+
+    [Required]
+    [Column("user_iban")]
+    [StringLength(21, MinimumLength = 21)]
+    public string Iban { get; set; }
+
+    [Column("user_phone")]
+    public string Phone { get; set; }
+
+    [Column("user_address")]
+    public string Address { get; set; }
+
+    [Required]
+    [Column("user_isActive")]
+    public bool IsActive { get; set; } = true; // SQLite utilise INTEGER 1/0
+
+    [Required]
+    [Column("user_createAt")]
+    public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+
+    [Required]
+    [Column("user_updateAt")]
+    public DateTime UpdateAt { get; set; } = DateTime.UtcNow;
 
 }
